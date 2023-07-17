6b41e7c8d4a56559833c2a3da64cb8a9d038bca8
kdhleppa <newkdh8624@gmail.com>
2023-07-17 09:47:16 +0900

    op1st

---
+++ src/edu/kh/op/ex/ExampleRun.java
@@ -0,0 +1,17 @@
+package edu.kh.op.ex;
+
+public class ExampleRun { // 코드 실행용 클래스
+	
+	// 메인메서드 필수 작성
+	public static void main(String[] args) {
+		
+		// OpExample 생성
+		// 같은 패키지 안에서는 import 필요 없음
+		OpExample ex = new OpExample();
+		
+		ex.ex1(); // ex가 가지고 있는 ex1() 메서드 실행
+		
+		
+	}
+
+}
